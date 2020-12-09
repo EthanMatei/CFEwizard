ef6caf652d296ea159efda730aaae833a84c0cf0
Jim Mullen <mullen2@iu.edu>
2020-12-09 13:02:27 -0500

    Added initial R scripts

---
+++ src/main/R/DEdiscovery.R
@@ -0,0 +1,447 @@
+
+
+
+wants <- c("RODBC","RODBCext","gWidgets", "tcltk", "plyr", "gtools", "dplyr")
+has   <- wants %in% rownames(installed.packages())
+if(any(!has)) install.packages(wants[!has])
+
+library(RODBC)
+require(gWidgets)
+require(tcltk)
+library(gtools)
+library(plyr)
+library(dplyr)
+options(guiToolkit="tcltk") 
+
+
+addAsymmetry <- TRUE
+
+
+#get access database location
+d <- "Z:\\Jim Mullen\\Discovery\\PheneVisit Database1.accdb"
+
+##open connection with access database
+PheneData <- odbcConnectAccess2007(d)
+source("Z:\\Jim Mullen\\Discovery\\accessIntegrationDE.R")
+
+##get processed access data
+accessIntegrationOutput <- prepAccessData(PheneData)
+PheneData <- accessIntegrationOutput[[1]]
+dxChoice <- accessIntegrationOutput[[2]]
+cohortChoice <- accessIntegrationOutput[[3]]
+rm(accessIntegrationOutput)
+
+PHENE <- "SI"   # This is your dependent variable
+
+DEdata <- read.csv("Z:\\Jim Mullen\\Discovery\\Normalization for 821  Suicide  based on mean and 2 HK genes (ABN 10-28-2020 SG 10-22-2020).csv") #location of RMA data #location of RMA data
+
+rownames(DEdata) <- DEdata[,1] #make the first row into headers
+DEdata <- DEdata[, -1] #delete the now-redundant first row
+
+#transpose data to set the subject visits as rows
+DEdata <- as.data.frame(t(DEdata))
+
+
+#define function that gets the last n characters of a string x
+substrRight <- function(x, n){
+  substr(x, nchar(x)-n+1, nchar(x))
+}
+
+
+DEdata <- cbind(DEdata,substrRight(rownames(DEdata),2)) #get visit number
+colnames(DEdata)[length(DEdata)] <- "VisitNumber" #name the column
+
+DEdata["PheneVisit"] <- rownames(DEdata) #make "PheneVisit" column from row IDs
+
+gc()
+
+
+stringColumns <- c("PheneVisit","Subject","date", PHENE)
+
+
+data <- join(DEdata,PheneData[stringColumns], #merge to get SI row
+             by = "PheneVisit", #merge on phene visit
+             type="inner") #drop unmatched (i.e., out-of-cohort) cases 
+
+
+rownames(data) <- as.matrix(data["PheneVisit"]) #make row names
+
+
+
+
+
+##add empty row to hold AP scores
+data <- rbind(data,rep(NA,ncol(data)))
+
+rownames(data)[nrow(data)] <- "DEscores"
+
+
+data[, ! names(data) %in% stringColumns] <- lapply(data[, ! names(data) %in% stringColumns], as.numeric)
+#set initial DEscore values to 0 on full data.frame
+data["DEscores", ! names(data) %in% stringColumns] <- 0
+
+
+#### test data for practice
+# i <- 1
+# x <- data[which(data$Subject == "phchp304"),]
+# x <- cbind(x[1070:1075], x[stringColumns])
+
+
+
+
+subjectDEscore <- data["DEscores",-((ncol(data)-3):ncol(data))] #this holds an subject-level APscore, drops identifier columns
+subjectLevelOutpout <- subjectDEscore
+
+loopNumber <- 1
+#put some calming messages here
+calmingMessage <- c("This will take awhile. Now is a good time to grab a coffee or a bite to eat",
+                    "You've got a great outfit on!",
+                    "You look very professional today",
+                    "You have kind eyes.",
+                    "Give yourself a meaningful compliment.",
+                    "Keep your head up!",
+                    "You're hard at work")
+
+list.of.unique.subjects <- as.character(unique ( data[-nrow(data) & order("PheneVisit"),"Subject"] ))
+list.of.unique.subjects <- list.of.unique.subjects[!is.na(list.of.unique.subjects)]
+
+
+
+wormShape <- subjectDEscore
+
+
+cat("You selected cohort",cohortChoice,"and diagnosis:",ifelse(dxChoice != "", dxChoice, "everyone"))
+
+numberOfSubjects <- length(unique ( data[-nrow(data),"Subject"] ))
+
+
+
+summaryResultsTable <- c("Subject",
+                   "Number of visits")
+
+
+
+for ( uniqueSubject in list.of.unique.subjects  ) { #loop through individual subject IDs
+  
+  
+  calmingMessageSelected <- sample(calmingMessage,1, replace=TRUE)
+  cat("
+      
+      Now processing DE scores for",uniqueSubject,"(", loopNumber,"of", numberOfSubjects,"total subjects )
+      
+      ",calmingMessageSelected,
+      "                             
+      
+      ")
+  loopNumber <- loopNumber + 1
+  
+  
+  x <- data[which(data["Subject"] == uniqueSubject),]
+  x  <-  x [ order( as.Date(x$date) ), ] # order by visit
+  
+  cat("\n...")
+  totalVisits <- nrow(x) #get number of visits for this subject
+  cat("\n...")
+  subjectDEscore["DEscores",] <- 0 #reset AP score to 0 for the new subject
+  cat("\n...")
+  
+  subjectDEscore["perfectionCounter",] <- 0 #reset perfectionCounter to 0 for the new subject
+  
+
+  
+  for (visit in 1:(totalVisits-1)){ ##stop calculating when there are no comparisons left
+    
+    
+    #status update  
+    cat("
+        
+        #######################################################
+        ##                                                   ## 
+        ## Processing subject", uniqueSubject, "visits", visit,"-",visit + 1, "( of",totalVisits,") ## 
+        ##                                                   ## 
+        #######################################################
+        
+        
+        
+        
+        ")
+    
+    
+    
+  
+    
+    pheneChange <- x[visit + 1,PHENE] - x[visit,PHENE] #change in phene state
+    
+    
+
+    
+    #changes in gene states (don't do this to factor columns)
+    cat("\nCalculating gene changes for",length(x) - length(stringColumns),"probe sets
+        ...")
+    geneChange <- x[visit + 1,! names(x) %in% stringColumns] / x[visit,! names(x) %in% stringColumns] 
+    
+    ## set fold changes. ORDER MATTERS here.
+    cat("\n\nConverting continuous fold changes to categorical values:
+Max changes downward")
+    geneChange[1,which((geneChange<=0.8333333333333333) & (1==1)) ] <- -1
+    cat("\nMarginal changes downward")
+    geneChange[1,which((geneChange<=0.90909090909090) & (geneChange>0.8333333333333333))] <- -0.5
+    cat("\nNo change")
+    geneChange[1,which((geneChange>0.90909090909090) & (geneChange<1.1))] <- 0
+    cat("\nMarginal changes upward")
+    
+    geneChange[1,which((geneChange>=1.1) & (geneChange<1.2))] <- 0.5
+    cat("\nMax changes upward\n")
+    
+    geneChange[1,which((geneChange>=1.2) & (1==1))] <- 1
+    cat("\nGet latest gene value and phene value
+        ...")
+    cat("\n...")
+    pheneValue <- x[visit + 1,PHENE]
+    
+ 
+    
+    
+    
+    
+    
+    if (visit == 1){
+      
+      lookAround <- data.frame(pheneChange)
+      
+
+      wormShape <- subjectDEscore[-(1:2), ]
+      
+    }else{
+      
+      lookAround <- rbind(lookAround, pheneChange)
+      
+    }
+    wormShape <- rbind(wormShape, geneChange)
+    
+    
+    
+    
+    if (totalVisits > 2){
+      cat("\nCalculating perfection
+          ...")
+      
+      
+      if ( pheneChange != 0 ){ #if there is a phene change, assign points based off of gene change direction
+        ## if the perfectionCounter value ends the loop equalling the number of comparisons, you have earned a perfection bonus
+        subjectDEscore["perfectionCounter",]  <-  subjectDEscore["perfectionCounter",]  + sign(geneChange[] * pheneChange)
+        cat("\n...")
+      }
+      
+      if ( pheneChange == 0 ) { #if there is no phene change, we need special logic to account for genes that didn't change
+        cat("\nThere was no phene change between these two visits, which necessitates some extra calculations")
+        
+  
+        
+      }
+      
+      
+    }else{
+      cat("\nNo need to calculate perfection score because this subject only has two visits")
+    }
+    
+    ##note: there are only two change conditions where APscore has to be adjusted
+    
+    cat("\nPhene and gene changes calculated\nNow calculating DE points for visits",visit,"-",visit+1)  
+    
+    if ( pheneChange == 1 ) { #if phene changes, add gene change value to APscore
+      
+      subjectDEscore["DEscores",] <- subjectDEscore["DEscores",] + geneChange[] 
+      cat("\n...")
+      
+    }
+    
+    if ( pheneChange == -1 ) { #if phene changes, add gene change value to APscore
+      
+      subjectDEscore["DEscores",] <- subjectDEscore["DEscores",] + (geneChange[] * -1)
+      cat("\n...")
+      
+    }
+    
+    if ( pheneChange == 0 ){ #if phene doesn't change, nor does gene...
+      cat("\nFor genes that didn't change on this visit, we can't assign points until we get through all the other visits")
+
+    }
+    
+    
+    gc()
+    
+    
+  }
+  
+    
+  if (totalVisits > 2){
+  
+    
+  for ( comparison in 1:nrow(lookAround)) {
+    
+    if ( lookAround[comparison, "pheneChange"]==0  ){
+      
+      temp <- subjectDEscore
+      temp["DEscores",] <- 0 #reset DE score to 0 for the new subject
+      cat("\n...")
+      
+      temp["perfectionCounter",] <- 0 #reset perfectionCounter to 0 for the new subject
+      
+      cat("\n\nFor comparison",comparison, "there was no phene change\n\nDaisychain: ENGAGE\n")
+
+       pheneValue <- x[comparison,PHENE] 
+
+      
+      cat("\nInitialize temporary points vector for this comparison")
+      pointsVector <- wormShape[1,]
+      pointsVector[1,] <- 0
+      
+      
+      if (comparison > 1){
+           for ( i in (comparison-1):1){
+            
+            if ( (i > 0) & (i != comparison) ){
+              
+            cat("\nLooking left to comparison",i)
+
+            if (pheneValue == 1 ){
+              tempWorm <- wormShape
+              if (addAsymmetry ){
+                tempWorm[i, (pointsVector == 0) & ( tempWorm[i,] == -0.5 )] <- -1
+                
+              }
+              
+            pointsVector[,pointsVector == 0 ] <-  tempWorm[i, pointsVector == 0]
+            }
+            
+            if (pheneValue == 0 ){
+              tempWorm <- wormShape
+              if (addAsymmetry ){
+         
+                tempWorm[i, (pointsVector == 0) & (tempWorm[i,] == -0.5 )] <- -1
+                
+              }
+
+              pointsVector[,pointsVector == 0 ] <- (tempWorm[i, pointsVector == 0])*-1
+            }
+            
+            
+            
+              }
+            }
+          }
+      
+      for ( i in (comparison+1):nrow(lookAround)){
+        
+        if ( (i < (nrow(lookAround)+1)) & (comparison != nrow(lookAround)  )){
+       
+          
+          cat("\nLooking right to comparison",i)
+          if (pheneValue == 1 ){
+            tempWorm <- wormShape
+            if (addAsymmetry ){
+              tempWorm[i, (pointsVector == 0) & (tempWorm[i,] == 0.5 )] <- 1
+            
+            }
+            
+            pointsVector[,pointsVector == 0 ] <- (tempWorm[i, pointsVector == 0])*-1
+
+          }
+          
+          if (pheneValue == 0 ){
+            tempWorm <- wormShape
+            if (addAsymmetry ){
+              tempWorm[i, (pointsVector == 0) & (tempWorm[i,] == 0.5 )] <- 1
+            
+            }
+            pointsVector[,pointsVector == 0 ] <- tempWorm[i, pointsVector == 0]
+          }
+        }
+      }
+      
+          comparisonGeneChanges <- wormShape[ comparison , ]
+      cat("\nManually assigning zero points wherever genes changed for comparison",comparison)
+      
+          pointsVector[,  comparisonGeneChanges != 0  ] <- 0   ##zero out the changed comparisons
+      
+      
+          
+          cat("\nCalculating exact points received")
+
+          temp["DEscores",] <- temp["DEscores",] + pointsVector
+                    
+          
+          cat("\nCorrecting perfection counters") 
+          
+      
+          temp["perfectionCounter", ] <-   temp["perfectionCounter", ]  +  sign(pointsVector)
+      
+      
+      subjectDEscore <-  subjectDEscore + temp
+      
+      
+  
+    }
+  
+  }
+  
+  }
+  
+  
+  if (totalVisits > 2){
+    
+    
+    cat("\n\nMarking genes with perfect scores for subject",uniqueSubject)
+    perfectionLogical <- (totalVisits-1) == abs( subjectDEscore["perfectionCounter",]  )
+    cat("\n...")
+    perfectionLogical[is.na(perfectionLogical)] <- FALSE
+    cat("\n...")
+    cat("\nApplying perfection bonus...")
+    
+    #perfection bonus
+    subjectDEscore[1,perfectionLogical] <- as.matrix(subjectDEscore[1,perfectionLogical])*2 
+    
+    cat("\n...")
+    
+  }
+  
+  
+  
+  data["DEscores",! names(data) %in% stringColumns] <-   data["DEscores",! names(data) %in% stringColumns] + subjectDEscore[1,]
+  
+  subjectLevelOutput <- rbind(subjectLevelOutpout, subjectDEscore[1,])
+  row.names(subjectLevelOutput)[[nrow(subjectLevelOutput)]] <- uniqueSubject 
+  
+  summaryResultsTable <- rbind(summaryResultsTable, c(uniqueSubject,totalVisits))
+  gc()
+  
+ 
+}
+
+
+
+
+    THISISYOUROUTPUT <- data["DEscores",]
+    THISISYOUROUTPUT <- t(THISISYOUROUTPUT)
+  
+  
+  
+outputFile <- paste("Z:\\Jim Mullen\\Discovery\\output",PHENE, cohortChoice, dxChoice, Sys.Date(),"Suicide.csv")
+write.csv(THISISYOUROUTPUT, outputFile)
+
+reportFile <- paste("Z:\\Jim Mullen\\Discovery\\output",PHENE, cohortChoice, dxChoice, Sys.Date()," REPORT SUMMARY.csv")
+write.csv(summaryResultsTable,reportFile)
+
+
+file.show(outputFile)
+file.show(reportFile)
+
+View(t(subjectLevelOutput))
+
+
+
+
+
+
+
---
+++ src/main/R/accessIntegrationDE.R
@@ -0,0 +1,155 @@
+
+
+
+
+
+prepAccessData <- function(data){
+  
+  #specify all the names of the tables you want to pull from access
+  cohortTbl <- "Cohorts"
+  tblsNeeded <- c("CFI-S","SAS","SMS","HAMD SI","Pain","PANSS", "Demographics","SSS", "Diagnosis", "Subject Identifiers", cohortTbl)
+  
+  varName <- NULL #instantiate varNames holder
+  
+  #pull all the sql keys to use as a base for merging
+  bigData <- sqlFetch(data,tblsNeeded[1])$PheneVisit
+  bigData <- data.frame(bigData)
+  colnames(bigData) <- "PheneVisit"
+  
+  
+  for (i in tblsNeeded){
+    # collect all the tables you want and merge them together sequentially
+    bigData <- merge(bigData,sqlFetch(data, i, na.strings=c("na","NA","", "<NA>", "Na", "N/A")), by="PheneVisit")
+  }
+  
+  #define function that gets all characters of string x minus the last n characters
+  substrLeft <- function(x, n){
+    substr(x, 1, nchar(x)-n)
+  }
+  
+  #define a "Subject" column that excludes visit data
+  bigData <- cbind(bigData,substrLeft( sapply(bigData["PheneVisit"],as.character),2))
+  colnames(bigData)[length(bigData)] <- "Subject"
+  
+
+
+  ##calculate raw SAS score
+  bigData["sasScore"] <- rowMeans(bigData[c("SAS Anxiety (0-100); ~ 4/14/11 don't reverse scores; 1st switch ",
+                                            "SAS Uncertainty (0-100)",
+                                            "SAS Fear (0-100)",
+                                            "SAS Anger (0-100)")])
+  ##calculate raw SMS score
+  bigData["smsScore"] <- rowMeans(bigData[c("SMS Mood (0-100)",
+                                           "SMS Motivation (0-100)",
+                                           "SMS Movement (0-100)",
+                                           "SMS Thinking (0-100)",
+                                           "SMS Self-esteem (0-100)",
+                                           "SMS Interest (0-100)",
+                                           "SMS Appetite (0-100)")])
+  
+  ##calculate raw SMSmood score
+  bigData["SMSmood"] <- rowMeans(bigData[c("SMS Mood (0-100)")])
+                                            
+  
+  ##calculate SASS composite
+  bigData["SASS"] <- bigData["sasScore"] - bigData["smsScore"]
+  
+  
+  ##calculate CFI score
+  bigData["cfiscore"] <- rowMeans(sapply(bigData[c("1 Psychiatric",
+                                            "2 Compliance",
+                                            "3 Familial",
+                                            "4 Example",
+                                            "5 Abuse",
+                                            "6 Medical",
+                                            "7 Losses",
+                                            "8 Useless",
+                                            "9 Introverted",
+                                            "10 Dissatisfied",
+                                            "11 Hopeless",
+                                            "12 Addiction",
+                                            "13 History",
+                                            "14 Non-religious",
+                                            "15 Rejection",
+                                            "16 Isolation",
+                                            "17 Impulsive",
+                                            "18 Non-coping",
+                                            "19 Childless",
+                                            "20 Hallucinations",
+                                            "21 Age",
+                                            "22  Gender")], as.numeric), na.rm=TRUE)
+  
+  
+
+  ##define Phene as people who scored a 2 or greater on the ideation item of HAMD
+  bigData["SI"] <- as.numeric(bigData["HAMD SI"] >= 2)
+  
+  bigData["STRESS"] <- as.numeric(bigData["SSS- Life Stress (0-100)"] >= 67)
+  
+  bigData["Anxiety"] <- as.numeric(bigData["SAS Anxiety (0-100); ~ 4/14/11 don't reverse scores; 1st switch "] >= 60)
+  
+  bigData["MOOD"] <- as.numeric(bigData["SMS Mood (0-100)"] >= 67)
+  
+  bigData["Appetite"] <- as.numeric(bigData["SMS Appetite (0-100)"] >= 60)
+  
+  ##Re-encode gender as a string ("M" or "F")
+  bigData["Gender"] <- lapply(bigData["Gender(M/F)"], as.character)
+  
+  bigData["PAIN"] <- as.numeric(bigData["Pain Scale"] >= 6)
+  
+  bigData["Delusions"] <- as.numeric(bigData["P1 Delusions (1-7)"] >= 4)
+  
+  bigData["Hallucinations"] <- as.numeric(bigData["P3 Hallucinations (1-7)"] >= 4)
+  
+  
+  #get names of cohorts
+  cohortColumns <- sqlColumns(data,cohortTbl)$COLUMN_NAME
+  #drop the subject header column
+  cohortColumns <- cohortColumns[-1]
+  
+  cohortString <- "" #initiate variable for prompt
+  for (i in 1:length(cohortColumns)){
+    entry <- paste0("(",i,") ",cohortColumns[i])
+    
+    cohortString <- paste(cohortString,entry, sep="\n")
+    
+    
+    }
+
+  cohortPreference <- ginput(message = paste("Enter the number corresponding with the cohort that you want to use.",cohortString), 
+                           icon="question",
+                           title="Choose a cohort")
+  
+  #make sure the user entered a choice of cohort
+  stopifnot(!(cohortPreference == ""))
+  
+  #Convert string to integer
+  cohortPreference <- strtoi(cohortPreference)
+  
+  #subset data by cohort
+  bigData <- bigData[bigData[cohortColumns[cohortPreference]] == 1,]
+  
+  dxCodeString <- ""
+  dxCode <- ginput(message = paste("Type the diagnosis code you want to use.\nIf you want to keep everyone, just press <enter>",dxCodeString), 
+                   icon="question",
+                   title="Choose a diagnosis")
+  
+  #make sure the user entered a choice of diagnosis
+  if (dxCode != ""){
+    bigData <- bigData[which(bigData["DxCode"]==dxCode),]
+  }
+  
+  
+  ##clear up ram
+  gc()
+  
+  #close database
+  close(data)
+  
+  output <- list(bigData, dxCode, cohortColumns[cohortPreference])
+  
+  
+  return(output)
+  
+  }
+  

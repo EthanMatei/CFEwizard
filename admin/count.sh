8f9a965959fa4af379b4c160275a048d68c49a76
Jim Mullen <mullen2@iu.edu>
2023-06-08 10:55:26 -0400

    Added count for lines of test code

--- admin/count.sh
+++ admin/count.sh
@@ -1,9 +1,13 @@
 # Script for counting the lines of code in the CFE Wizard
 echo "CFE Wizard - count of lines of code"
-cloc --quiet --hide-rate --exclude-ext=md \
+cloc --quiet --hide-rate --exclude-ext=md,html \
     ../src/main/java/cfe \
     ../src/main/resources/R \
     ../src/main/resources/python \
     ../src/main/webapp/pages
 
+
+echo "CFE Wizard - count of lines of test code"
+cloc --quiet --hide-rate --exclude-ext=md \
+    ../src/test/java/cfe
 

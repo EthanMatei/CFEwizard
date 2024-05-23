130d5380ab2731c9242567a37896bf1786aa3669
Jim Mullen <mullen2@iu.edu>
2024-05-23 14:25:58 -0400

    Updated development version number

--- src/main/java/cfe/model/VersionNumber.java
+++ src/main/java/cfe/model/VersionNumber.java
@@ -1,5 +1,5 @@
 package cfe.model;
 
 public class VersionNumber {
-    public static final String VERSION_NUMBER = "1.0.0.10-dev";
+    public static final String VERSION_NUMBER = "1.0.0.11-dev";
 }

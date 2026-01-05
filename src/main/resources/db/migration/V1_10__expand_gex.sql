cfefcea7b8ac7aebbb62b5b1c16267098cff69d5
jbalducci03 <jbalducci@arizona.edu>
2026-01-05 16:17:33 -0700

    Expand VARCHAR fields in Gene-Expression literature databases

---
+++ src/main/resources/db/migration/V1_10__expand_gex.sql
@@ -0,0 +1,8 @@
+ALTER TABLE hubraingex MODIFY relevantDisorder VARCHAR(255);
+ALTER TABLE hubraingex MODIFY subDomain VARCHAR(255);
+
+ALTER TABLE nhbraingex MODIFY relevantDisorder VARCHAR(255);
+ALTER TABLE nhbraingex MODIFY subDomain VARCHAR(255);
+
+ALTER TABLE nhpergex MODIFY relevantDisorder VARCHAR(255);
+ALTER TABLE nhpergex MODIFY subDomain VARCHAR(255);

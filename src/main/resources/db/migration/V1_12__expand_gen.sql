c1bedec9d8915e6ec5550ac56da85b8a44630843
jbalducci03 <jbalducci@arizona.edu>
2026-02-24 13:50:51 -0700

    Expand VARCHAR size for relevantDisorder and subDomain for Genetic DB

---
+++ src/main/resources/db/migration/V1_12__expand_gen.sql
@@ -0,0 +1,17 @@
+ALTER TABLE hugenassoc MODIFY relevantDisorder VARCHAR(255);
+ALTER TABLE hugenassoc MODIFY subDomain VARCHAR(255);
+
+ALTER TABLE nhgenassoc MODIFY relevantDisorder VARCHAR(255);
+ALTER TABLE nhgenassoc MODIFY subDomain VARCHAR(255);
+
+ALTER TABLE hugencnv MODIFY relevantDisorder VARCHAR(255);
+ALTER TABLE hugencnv MODIFY subDomain VARCHAR(255);
+
+ALTER TABLE nhgencnv MODIFY relevantDisorder VARCHAR(255);
+ALTER TABLE nhgencnv MODIFY subDomain VARCHAR(255);
+
+ALTER TABLE hugenlinkage MODIFY relevantDisorder VARCHAR(255);
+ALTER TABLE hugenlinkage MODIFY subDomain VARCHAR(255);
+
+ALTER TABLE nhgenlinkage MODIFY relevantDisorder VARCHAR(255);
+ALTER TABLE nhgenlinkage MODIFY subDomain VARCHAR(255);
